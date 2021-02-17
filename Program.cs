1eec8d5bcbfdbc60392aa8c03e4df46f19816673
MertAhMeDoV34 <79190600+MertAhMeDoV34@users.noreply.github.com>
2021-02-17 10:04:38 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Въведи двоично число: ");
+            string bin = Console.ReadLine();
+            int dec = Convert.ToInt32(bin, 2);
+            Console.WriteLine("Десетично:" +dec);
+            Console.WriteLine("Двоично:" +bin);
+        }
+    }
+}
